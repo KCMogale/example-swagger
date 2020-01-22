e043c0d5cd4dc5ff3ca357bd32dbfd64321c4ed4
Koboti Mogale <koboti.mogale@fnb.co.za>
2020-01-22 15:52:04 +0200

    New Commit

---
+++ swagger/example-swagger/src/main/java/co/za/koboti/exampleswagger/ExampleSwaggerApplication.java
@@ -0,0 +1,13 @@
+package co.za.koboti.exampleswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSwaggerApplication.class, args);
+	}
+
+}
